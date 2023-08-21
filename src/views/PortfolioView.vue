2ded6e37a223d71e170ef79ba67cf0a0cb1fcd10
helioswan <helios.wan@hotmail.fr>
2023-08-21 12:32:07 +0200

    Add lastest and proejcts sections to portfolio view

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -2,7 +2,9 @@
   <Header />
   <main>
     <Hero />
-    <ProjectDeck title="Favorite projects" :projects="projects" />
+    <ProjectDeck title="Favorite projects" :projects="favProjects" />
+    <ProjectDeck title="Lastest projects" :projects="lastestProjects" />
+    <ProjectDeck title="Projects" :projects="projects" />
   </main>
 </template>
 
@@ -10,6 +12,76 @@
 import Header from "../components/Common/Header.vue";
 import Hero from "../components/Common/Hero.vue";
 import ProjectDeck from "../components/Project/ProjectDeck.vue";
+
+const favProjects = [
+  {
+    title: "Title",
+    desc: "Desc",
+    shortDesc: "Short desc",
+    imgUrl: "",
+    githubLink: "Github",
+    liveLink: "Live",
+  },
+  {
+    title: "Title",
+    desc: "Desc",
+    shortDesc: "Short desc",
+    imgUrl: "",
+    githubLink: "Github",
+    liveLink: "Live",
+  },
+  {
+    title: "Title",
+    desc: "Desc",
+    shortDesc: "Short desc",
+    imgUrl: "",
+    githubLink: "Github",
+    liveLink: "Live",
+  },
+  {
+    title: "Title",
+    desc: "Desc",
+    shortDesc: "Short desc",
+    imgUrl: "",
+    githubLink: "Github",
+    liveLink: "Live",
+  },
+];
+
+const lastestProjects = [
+  {
+    title: "Title",
+    desc: "Desc",
+    shortDesc: "Short desc",
+    imgUrl: "",
+    githubLink: "Github",
+    liveLink: "Live",
+  },
+  {
+    title: "Title",
+    desc: "Desc",
+    shortDesc: "Short desc",
+    imgUrl: "",
+    githubLink: "Github",
+    liveLink: "Live",
+  },
+  {
+    title: "Title",
+    desc: "Desc",
+    shortDesc: "Short desc",
+    imgUrl: "",
+    githubLink: "Github",
+    liveLink: "Live",
+  },
+  {
+    title: "Title",
+    desc: "Desc",
+    shortDesc: "Short desc",
+    imgUrl: "",
+    githubLink: "Github",
+    liveLink: "Live",
+  },
+];
 
 const projects = [
   {
