60bb7994c0ee3348885223e8385d5b94525eb971
helioswan <helios.wan@hotmail.fr>
2024-03-14 15:52:36 +0100

    Fix spelling mistake

--- src/components/Resume/AboutMe.vue
+++ src/components/Resume/AboutMe.vue
@@ -8,18 +8,18 @@
           <span class="font-bold">Hello! Je m'appelle Hélios Wan.</span>
           <br /><br />
           Je suis un développeur Front end, passionné par la création
-          d'expériences utilisateur fluides et accessibles pour tous.
+          d'expériences utilisateurs fluides et accessibles pour tous.
           <br />
           Actuellement en alternance chez Capgemini, j'ai la chance de
           travailler sur des projets me permettant d’approfondir mes
           connaissances sur le domaine du développement web.
           <br />
           Je suis également sensible à l'accessibilité et je m'assure de rendre
-          mes projets accessible à tous les utilisateurs.
+          mes projets accessibles à tous les utilisateurs.
           <br />
           Mes années de développement Freelance et mes stages m’ont permis
           d’améliorer mes compétences dans la création de sites web performants,
-          responsive et accessible.
+          responsives et accessibles.
           <br />
           Enfin, je suis toujours à la recherche de nouveaux défis et de
           nouvelles opportunités pour améliorer mes compétences et mes
